f5ec2f3193b4cb177e2b3147d55b271383310561
Hunter Chambers <hmchambe@mtu.edu>
2019-10-31 15:26:04 -0400

    Just backing up

--- thread.cpp
+++ thread.cpp
@@ -1,11 +1,11 @@
 #include  <iostream>
 #include "thread.h"
 
-PrefixSumThread::PrefixSumThread(int Lowerbound, int Upperbound, int Array[]) 
-          :lowerbound(Lowerbound), upperbound(Upperbound), a(Array)
+PrefixSumThread::PrefixSumThread(int Column, int Row) 
+          :column(Column), row(Row)
 {
      ThreadName.seekp(0, ios::beg);
-     ThreadName << "Sorting" 
+     ThreadName << "sum" 
                 << '(' << Lowerbound << ':' << Upperbound << ')'
                 << '\0';
 }
@@ -13,39 +13,9 @@ PrefixSumThread::PrefixSumThread(int Lowerbound, int Upperbound, int Array[])
 void PrefixSumThread::ThreadFunc()
 {
      Thread::ThreadFunc();
-     int pivot = a[upperbound];         // pick up pivot value
-     int left = lowerbound - 1,         // scan index from left side
-         right = upperbound,            // scan index from right side
-         tmp, pivotIndex;
-
-     PrefixsumThread  *leftSortThread,  // recursive sorting threads
-                     *rightSortThread;
+ 	
 
 
-     if (lowerbound >= upperbound)
-           Exit();                      //return and recursion ends
 
-      while (left < right) {            // partition loop
-          do { left++;} while (a[left] < pivot);
-          do { right--;} while (a[right] > pivot);
-          if (left < right ) {          
-               tmp = a[left];
-               a[left] = a[right];
-               a[right] = tmp;
-          }
-     }
-
-     pivotIndex = left;                 // put the  pivot back
-     tmp = a[pivotIndex];
-     a[pivotIndex] = pivot;
-     a[upperbound] = tmp;
-
-     // start the "recursive threads"
-     leftSortThread = new PrefixSumThread(lowerbound, pivotIndex - 1, a);
-     leftSortThread->Begin();
-     rightSortThread = new PrefixSumThread(pivotIndex + 1, upperbound, a);
-     rightSortThread->Begin();
-     leftSortThread->Join();            // wait for the child threads
-     rightSortThread->Join();
-     Exit();                            // done and exit
+  Exit();                            // done and exit
 }
--- thread.h
+++ thread.h
@@ -1,13 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
 #include "ThreadClass.h"
+
+extern int B[512][512]
 
 class PrefixSumThread : public Thread
 {
      public:
           // constructor
-     QuickSortThread(int Lowerbound, int Upperbound, int Array[]); 
-	  private:
-	  void ThreadFunc();  // thread body
-	  int  lowerbound;    // lower bound of the sub-array to be sorted
-	  int  upperbound;    // upper bound of the sub-array to be sorted
-	  int  *a;            // pointing to array to be sorted
+     PrefixSumThread(int column, int row); 
+     
+     private:
+     void ThreadFunc();  // thread body
+     int row;
+     int column;
 };
